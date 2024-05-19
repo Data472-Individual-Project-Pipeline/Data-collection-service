43ab84ddf05bee72d6f78f5931c467b07756af79
Hua <hwa205@uclive.ac.nz>
2024-05-19 11:29:02 +0000

    added tay51 whole datasets collection processos

--- dags/modules/__init__.py
+++
@@ -1,1 +0,0 @@
-# This file is intentionally left blank to indicate that the directory is a Python package
--- dags/tya51.py
+++ dags/tya51.py
@@ -1,12 +1,12 @@
+import sys
+import os
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from modules.tya51_data_processor import Tya51DataProcessor
 import logging
 
-root_name = 'tya51'
-location_url = 'http://13.236.194.130/tya51/api/river/location'
-base_url = 'http://13.236.194.130/tya51/api/river/observation'
+# Import the custom data processor
+from processors.tya51_processor import Tya51Processor
 
 default_args = {
     'owner': 'airflow',
@@ -26,11 +26,13 @@ dag = DAG(
     catchup=False,
 )
 
+location_url = 'http://13.236.194.130/tya51/api/river/location'
+observation_base_url = 'http://13.236.194.130/tya51/api/river/observation'
+owner = 'tya51'
+
 def create_and_check_tables():
     logging.info("Creating and checking tables")
-    processor = Tya51DataProcessor(postgres_conn_id='postgres_data472')
-
-    # Check and create location table
+    processor = Tya51Processor(postgres_conn_id='postgres_data472')
     location_table_schema = """
     CREATE TABLE IF NOT EXISTS tya51_location (
         locationId VARCHAR PRIMARY KEY,
@@ -43,9 +45,6 @@ def create_and_check_tables():
         inserted_at TIMESTAMP
     );
     """
-    processor.check_and_create_table('tya51_location', location_table_schema)
-
-    # Check and create observation table
     observation_table_schema = """
     CREATE TABLE IF NOT EXISTS tya51_observation (
         locationId VARCHAR,
@@ -57,26 +56,12 @@ def create_and_check_tables():
         PRIMARY KEY (locationId, timestamp)
     );
     """
-    processor.check_and_create_table('tya51_observation', observation_table_schema)
+    processor.create_tables(location_table_schema, observation_table_schema)
 
 def insert_data():
     logging.info("Inserting data")
-    processor = Tya51DataProcessor(postgres_conn_id='postgres_data472')
-
-    # Fetch location data
-    location_data = processor.fetch_data(location_url)
-    
-    # Insert location data
-    processor.insert_data('tya51_location', location_data, root_name)
-
-    # Fetch observation data
-    observation_data = try_fetch_data_for_days(processor, base_url)
-    
-    # Insert observation data
-    if observation_data:
-        processor.insert_data('tya51_observation', observation_data, root_name)
-    else:
-        logging.warning("No observation data to insert")
+    processor = Tya51Processor(postgres_conn_id='postgres_data472')
+    processor.process(location_url, observation_base_url, owner)
 
 # Task to create and check tables
 create_and_check_tables_task = PythonOperator(
