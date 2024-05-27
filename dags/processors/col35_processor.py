cb5e6ef07a3f5f30f1e9280672d5c5955fbb352b
Jacob Reid <jacob@sysint.co.nz>
2024-05-27 12:58:53 +1200

    added more table data for testing on col35

--- dags/processors/col35_processor.py
+++ dags/processors/col35_processor.py
@@ -58,11 +58,20 @@ class Col35Processor:
                     inserted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     owner TEXT                                                  
                )
-               CREATE TABLE IF NOT EXISTS prisoner_security_class (
+               CREATE TABLE IF NOT EXISTS prisoner_number_sentences (
+                    id SERIAL PRIMARY KEY,
+                    city TEXT,
+                    date DATE,
+                    observations FLOAT64,
+                    type TEXT,
+                    inserted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    owner TEXT                     
+               )
+                CREATE TABLE IF NOT EXISTS prisoner_sentence_type (
                     id SERIAL PRIMARY KEY,
                     date DATE,
-                    securty_class TEXT,
                     observations FLOAT64,
+                    type TEXT,
                     inserted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                     owner TEXT                     
                )
@@ -117,6 +126,8 @@ class Col35Processor:
             cursor.close()
             conn.close()
         # second header
+        conn = self.hook.get_conn()
+        cursor = conn.cursor()
         try:
             count = 0
             duplicate_count = 0
@@ -145,6 +156,180 @@ class Col35Processor:
                         item['Age'],
                         item['Date'],
                         item['Observations'],
+                        owner
+                    ))
+                    count += 1
+            conn.commit()
+            self.logger.info(f"{count} items inserted.")
+            self.logger.info(f"{duplicate_count} items were duplicates and not inserted.")
+        except Exception as e:
+            self.logger.error(f"Error inserting items: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+        # third connector
+        conn = self.hook.get_conn()
+        cursor = conn.cursor()
+        try:
+            count = 0
+            duplicate_count = 0
+            # modify for this selection because each field is different.
+
+            for item in items['Gender']:
+                cursor.execute("""
+                    SELECT 1 FROM prisoner_population WHERE 
+                        date = %s AND 
+                        gender = %s AND 
+                        observations = %s
+                """, (
+                    item['Date'],
+                    item['Gender'],
+                    item['Observations']
+                ))
+
+                if cursor.fetchone():
+                    duplicate_count += 1
+                else:
+                    cursor.execute("""
+                        INSERT INTO prisoner_population (
+                            date, gender, observations, owner
+                        ) VALUES (%s, %s, %s, %s)
+                    """, (
+                        item['Date'],
+                        item['Gender'],
+                        item['Observations'],
+                        owner
+                    ))
+                    count += 1
+            conn.commit()
+            self.logger.info(f"{count} items inserted.")
+            self.logger.info(f"{duplicate_count} items were duplicates and not inserted.")
+        except Exception as e:
+            self.logger.error(f"Error inserting items: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+        # NoOfSentences
+        conn = self.hook.get_conn()
+        cursor = conn.cursor()
+        try:
+            count = 0
+            duplicate_count = 0
+            # modify for this selection because each field is different.
+            for item in items['NoOfSentences']:
+                if not item['Type'] == 'Total':
+                    cursor.execute("""
+                                    SELECT 1 FROM prisoner_number_sentences WHERE
+                                        city = %s AND 
+                                        date = %s AND 
+                                        observations = %s AND 
+                                        type = %s
+                                """, (
+                        item['City'],
+                        item['Date'],
+                        item['Observations'],
+                        item['Type']
+
+                    ))
+
+                    if cursor.fetchone():
+                        duplicate_count += 1
+                    else:
+                        cursor.execute("""
+                                        INSERT INTO prisoner_number_sentences (
+                                            city, date, observations, type, owner
+                                        ) VALUES (%s, %s, %s, %s, %s)
+                                    """, (
+                            item['City'],
+                            item['Date'],
+                            item['Observations'],
+                            item['Type'],
+                            owner
+                        ))
+                        count += 1
+                    conn.commit()
+                    self.logger.info(f"{count} items inserted.")
+                    self.logger.info(f"{duplicate_count} items were duplicates and not inserted.")
+        except Exception as e:
+            self.logger.error(f"Error inserting items: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+        # OffenceType
+        conn = self.hook.get_conn()
+        cursor = conn.cursor()
+        try:
+            count = 0
+            duplicate_count = 0
+            # modify for this selection because each field is different.
+
+            for item in items['OffenceType']:
+                cursor.execute("""
+                        SELECT 1 FROM prisoner_offence_type WHERE 
+                            date = %s AND 
+                            observations = %s AND
+                            offence_type = %s
+                    """, (
+                    item['Date'],
+                    item['Observations'],
+                    item['Offence type']
+                ))
+
+                if cursor.fetchone():
+                    duplicate_count += 1
+                else:
+                    cursor.execute("""
+                            INSERT INTO prisoner_offence_type (
+                                date, observations, offence_type, owner
+                            ) VALUES (%s, %s, %s, %s)
+                        """, (
+                        item['Date'],
+                        item['Observations'],
+                        item['Offence type'],
+                        owner
+                    ))
+                    count += 1
+            conn.commit()
+            self.logger.info(f"{count} items inserted.")
+            self.logger.info(f"{duplicate_count} items were duplicates and not inserted.")
+        except Exception as e:
+            self.logger.error(f"Error inserting items: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+        # Type (sentence_type)
+        conn = self.hook.get_conn()
+        cursor = conn.cursor()
+        try:
+            count = 0
+            duplicate_count = 0
+            # modify for this selection because each field is different.
+
+            for item in items['Type']:
+                cursor.execute("""
+                        SELECT 1 FROM prisoner_sentence_type WHERE 
+                            date = %s AND 
+                            observations = %s AND
+                            type = %s
+                    """, (
+                    item['Date'],
+                    item['Observations'],
+                    item['Type']
+                ))
+
+                if cursor.fetchone():
+                    duplicate_count += 1
+                else:
+                    cursor.execute("""
+                            INSERT INTO prisoner_offence_type (
+                                date, observations, type, owner
+                            ) VALUES (%s, %s, %s, %s)
+                        """, (
+                        item['Date'],
+                        item['Observations'],
+                        item['Type'],
                         owner
                     ))
                     count += 1
